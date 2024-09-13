cf13d78e1af68b2650317fc5567403e1d8ef7a48
Matthew Desabelle <desabellematthew@gmail.com>
2024-09-13 13:45:22 +0800

    initial commit

---
+++ CowsayProcess.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+
+/// <summary>
+/// Cowsay is a program that generates ASCII pictures of a cow with a message.
+/// </summary>
+class Cowsay
+{
+    /// <summary>
+    /// Generates a cow with a message.
+    /// </summary>
+    ///
+    /// <param name="message">The message to display.</param>
+    /// <returns>The ASCII picture of a cow with the message.</returns>
+    ///
+    /// <exception cref="Exception">If cowsay fails to generate the cow.</exception>
+    public static string Say(string message)
+    {
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = "/usr/games/cowsay",
+            RedirectStandardError = true,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+        };
+
+        using Process process = new()
+        {
+            StartInfo = startInfo,
+        };
+
+        Process.Start("/usr/games/cowsay", "dfs");
+        process.StandardInput.WriteLine(message);
+        process.StandardInput.Close();
+
+        string output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            string error = process.StandardError.ReadToEnd();
+            throw new Exception(error);
+        }
+
+        return output;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿﻿using System.ComponentModel;
+
+try
+{
+    Console.Write("-> Tell me what you want to say: ");
+    string? message = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(message))
+    {
+        Console.WriteLine("error: message cannot be empty.");
+        return;
+    }
+
+    string response = Cowsay.Say(message);
+    Console.WriteLine(response);
+}
+catch (Win32Exception)
+{
+    Console.WriteLine("error: cowsay is not installed on this system.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"error: {ex.Message}");
+}
